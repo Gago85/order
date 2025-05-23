de36afd0843c7e6b00499f8bc2b3ab0a371a94fb
Gago85 <goodbuns2017@outlook.com>
2025-05-23 12:46:05 +0300

    Update goodbuns_order_server.py

--- goodbuns_order_server.py
+++ goodbuns_order_server.py
@@ -72,4 +72,4 @@ def handle_order():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(host="0.0.0.0", port=10000)
